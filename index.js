792d92db6108570c3aa4af8e16067b79a8dd9685
steveyang-bolt <steve.yang@bolt.com>
2020-04-13 16:42:57 -0400

    log req body

--- index.js
+++ index.js
@@ -15,15 +15,16 @@ app.all('/*', function(req, res, next) {
     next();
 });
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
+// Router Section
 var router = express.Router()
 router.post("/universal", (req, res) => {
   console.log(req.body);
   res.json(respJsonFile);
 });
 
-// express-winston logger makes sense BEFORE the router
+// Middleware Section
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 app.use(expressWinston.logger({
   transports: [
     new winston.transports.Console()
@@ -33,12 +34,12 @@ app.use(expressWinston.logger({
     winston.format.json()
   ),
   meta: true, // optional: control whether you want to log the meta data about the request (default to true)
-  //msg: "HTTP {{req.method}} {{req.url}} {{req.body}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+  msg: "HTTP {{req.method}} {{req.url}} {{req.body}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
   expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
   colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
   ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
 }));
-
-
 app.use(router)
+
+
 app.listen(PORT);
